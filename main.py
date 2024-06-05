5297c597c6d6a36a43b4314024309e2e483660f7
Emma Systemics <emma.nworb@gmail.com>
2024-06-05 01:19:28 -0700

    Fixed

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+import os
+from dotenv import load_dotenv
 import torch
 from diffusers import DiffusionPipeline, AutoencoderKL
 
-vae = AutoencoderKL.
+load_dotenv()
+
+hugging_face_token = os.getenv('HUGGING_FACE_TOKEN')
+
+vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+pipe = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    vae=vae,
+    torch_dtype=torch.float16,
+    variant="fp16",
+    use_safetensors=True
+)
+
+pipe.load_lora_weights('nworb-ucsb/face_LoRA',)
+_ = pipe.to("cuda")
+
+prompt = "happy" # @param
+
+image = pipe(prompt=prompt, num_inference_steps=25).images[0]
+image.save("output_image.png")
